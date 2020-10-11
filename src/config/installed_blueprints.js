7b5b0fb8134e874dbe9213ec64805e25eefde6d9
Crowdbotics <team@crowdbotics.com>
2020-10-11 23:53:03 +0000

    add package blueprint-react-native-blank-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,5 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
+{ name: 'BlankScreen4138297', human_name: 'Blank Screen 4', access_route: 'BlankScreen4138297'},
 { name: 'BlankScreen3138295', human_name: 'Blank Screen 3', access_route: 'BlankScreen3138295'},
 { name: 'BlankScreen2138294', human_name: 'Blank Screen 2', access_route: 'BlankScreen2138294'},
 { name: 'BlankScreen6138293', human_name: 'Blank Screen 6', access_route: 'BlankScreen6138293'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import BlankScreen4138297Navigator from '../features/BlankScreen4138297/navigator';
 import BlankScreen3138295Navigator from '../features/BlankScreen3138295/navigator';
 import BlankScreen2138294Navigator from '../features/BlankScreen2138294/navigator';
 import BlankScreen6138293Navigator from '../features/BlankScreen6138293/navigator';
@@ -18,6 +19,7 @@ import BlankScreen3138289Navigator from '../features/BlankScreen3138289/navigato
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+BlankScreen4138297: { screen: BlankScreen4138297Navigator },
 BlankScreen3138295: { screen: BlankScreen3138295Navigator },
 BlankScreen2138294: { screen: BlankScreen2138294Navigator },
 BlankScreen6138293: { screen: BlankScreen6138293Navigator },
